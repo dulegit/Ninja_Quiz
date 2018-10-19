76054d4d132b927312d30738e145a1338186d04a
Dušan Andrejević <dule@Dusans-MacBook-Pro.local>
2018-10-19 02:42:59 +0200

    2018-10-19, Update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Ninja Quiz
+# Ninja Quiz
 ----------
 Short quiz with 10 questions about Superheroes developed in functional JavaScript...
+
+### 2018-10-19, Update
+* For `$form.addEventListener()` add external function `btnClicked()`
+* Add missing `removeEventListener()` method for `btnClicked()`, which missing cause error when starting game for second, third, (etc.) time, without reloading browser.
+    * In `function check(answer)` **question** object was undefined
+    * When you click on button to choose answer, it calls `check(event.target.value)` as much times as you start new game without reloading browser
+    * remove unnecessary code, optimize code
--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ var quiz = {
 		{"question" : "Superman", "answer" : "Clark Kent", "asked": false},
 		{"question" : "Batman", "answer" : "Bruce Wayne", "asked": false},
 		{"question" : "Wonder Woman", "answer" : "Dianna Prince", "asked": false},
-		{"question" : "Bananaman", "answer" : "Erik Banana", "asked": false},
-		{"question" : "Spiderman", "answer" : "Covek pauk", "asked": false},
+		{"question": "Black Panther", "answer": "T'Challa", "asked": false},
+		{"question" : "Spider-Man", "answer" : "Peter Parker", "asked": false},
 		{"question": "Catwoman", "answer": "Selina Kyle", "asked": false},
 		{"question": "The Hulk", "answer": "Bruce Banner", "asked": false},
 		{"question": "Iron Man", "answer": "Tony Stark", "asked": false},
@@ -25,39 +25,6 @@ var $feedback = document.getElementById("feedback");
 var $start = document.getElementById("start");
 var $form = document.getElementById("answer");
 var $timer = document.getElementById("timer");
-var $wait = document.getElementById("wait");
-
-//// view functions ////
-function update(element, content, klass) {
-	var p = element.firstChild || document.createElement("p");
-	p.textContent = content;
-	element.appendChild(p);
-	if (klass) {
-		p.className = klass;
-	}
-}
-
-function hide(element) {
-	element.style.display = "none";
-}
-
-function show(element) {
-	element.style.display = "block";
-}
-
-// Event Listeners
-$start.addEventListener('click', function(){play(quiz);}, false);
-
-// when you answer on question, 'asked' got a property 'true'. This function return 'asked' to be false
-function askedToFalse(){
-	for (var i = 0; i < quiz.questions.length; i++) {
-		quiz.questions[i].asked = false;
-	}
-}
-
-// hide the form at the start of the game
-hide($form);
-hide($wait);
 
 //// function definitions /////
 function random(a,b,callback){
@@ -71,21 +38,35 @@ function random(a,b,callback){
 	}
 	return result;
 }
+// when you answer on question, 'asked' got a property 'true'. This function return 'asked' to be false
+function askedToFalse(){
+	for (var i = 0; i < quiz.questions.length; i++) {
+		quiz.questions[i].asked = false;
+	}
+}
+// view functions
+function update(element, content, klass) {
+	var p = element.firstChild || document.createElement("p");
+	p.textContent = content;
+	element.appendChild(p);
+	if (klass) {
+		p.className = klass;
+	}
+}
+// hide element
+function hide(element) {
+	element.style.display = "none";
+}
+// show element
+function show(element) {
+	element.style.display = "block";
+}
 
-/// Loader ///
-var cl = new CanvasLoader('canvasloader-container');
-cl.setColor('#d64e36'); // default is '#000000'
-cl.setShape('spiral'); // default is 'oval'
-cl.setDiameter(44); // default is 40
-cl.setDensity(53); // default is 40
-cl.setRange(0.7); // default is 1.3
-cl.show(); // Hidden by default
+// Event Listeners
+$start.addEventListener('click', function(){play(quiz);}, false);
 
-// This bit is only for positioning - not necessary
-var loaderObj = document.getElementById("canvasLoader");
-loaderObj.style.position = "absolute";
-loaderObj.style["top"] = cl.getDiameter() * -0.5 + "px";
-loaderObj.style["left"] = cl.getDiameter() * -0.5 + "px";
+// hide the form at the start of the game
+hide($form);
 
 // PLAY FUNCTION
 function play(quiz) {
@@ -94,27 +75,44 @@ function play(quiz) {
 	hide($start);
 	hide($feedback);
 	hide($question);
-	hide($wait);
 	show($form);
 	show($timer);
 
-	$form.addEventListener("click", function(event){
-		event.preventDefault();
-		check(event.target.value);
-	}, false);
-
-	var score = 0; //initialize score
+	//initialize score
+	var score = 0;
 	update($score, score);
 
-	// main game loop
-	var question;
+	// define question variable and call func
+	var question = null;
 	chooseQuestion();
-	// end of main game loop
 
 	//initialize timer and set up an interval that counts down
-	var time = 20;
+	var time = 30;
 	update($timer, time);
+
 	var interval = window.setInterval(countDown, 1000);
+	$form.addEventListener("click", btnClicked);
+
+	function btnClicked(event) {
+		event.preventDefault();
+		check(event.target.value);
+	}
+
+	function check(answer) {
+		
+		console.log("check() invoked");
+		if(answer === question.answer) { // quiz[i][1] is the answer
+			update($feedback, "Correct!", "right");
+			show($feedback);
+			score++;
+			update($score, score);
+		} else {
+			update($feedback, "Wrong!", "wrong");
+			show($feedback);
+		}
+		$form.innerHTML = "";
+		chooseQuestion();
+	}
 
 	function chooseQuestion(){
 		
@@ -124,9 +122,9 @@ function play(quiz) {
 		});
 		// set the current question
 		question = questions[random(questions.length) - 1];
-		console.log(questions.length);
+		console.log('questions left: ', questions.length);
 		if(questions.length < 1){
-			console.log("BROJ PONUDJENIH PITANJA JE MANJI OD 1");
+			console.log("number of unanswered questions is less than 1");
 			gameOver();	
 		} else {
 			ask(question);
@@ -150,7 +148,6 @@ function play(quiz) {
 		options.forEach(function(name){
 			button = document.createElement("button");
 			button.value = name;
-			//button.className = "name";
 			button.textContent = name;
 			$form.appendChild(button);
 
@@ -166,39 +163,13 @@ function play(quiz) {
 		}
 	}
 
-	function check(answer) {
-		
-		console.log("check() invoked");
-		if(answer === question.answer) { // quiz[i][1] is ith answer
-			update($feedback, "Correct!", "right");
-			show($feedback);
-			// increase score by 1
-			score++;
-			update($score, score);
-		} else {
-			update($feedback, "Wrong!", "wrong");
-			show($feedback);
-		}
-		$form.innerHTML = "";
-		if (question === quiz.question.length) {
-			gameOver();
-		} else {
-			chooseQuestion();
-		}
-	}
-
 	// this is called every second and decreases the time
 	function countDown(){
-		// decrease time by 1
 		time --;
-		// update the time displayed
 		update($timer, time);
-		// the game is over if the timer has reached 0
+
 		if (time <= 0){
-			show($wait);
 			hide($timer);
-			hide($form);
-			return check();
 			gameOver();
 		}
 	}
@@ -211,10 +182,11 @@ function play(quiz) {
 		window.clearInterval(interval);
 		askedToFalse();
 		hide($form);
-		hide($wait);
 		hide($feedback);
 		show($question);
 		show($start);
+		$form.innerHTML = "";
+		$form.removeEventListener("click", btnClicked);
 	}
 }
 // END OF PLAY FUNCTION
